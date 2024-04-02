c6cb6fb27ee791e1933d625d90e9109434392ad4
Unstream <git@wont.stream>
2024-04-02 17:04:52 -0400

    move to bun

---
+++ src/root.tsx
@@ -0,0 +1,33 @@
+import { component$ } from "@builder.io/qwik";
+import {
+  QwikCityProvider,
+  RouterOutlet,
+  ServiceWorkerRegister,
+} from "@builder.io/qwik-city";
+import { RouterHead } from "./components/router-head/router-head";
+
+import "./sakura/scss/sakura-dark.scss";
+
+export default component$(() => {
+  /**
+   * The root of a QwikCity site always start with the <QwikCityProvider> component,
+   * immediately followed by the document's <head> and <body>.
+   *
+   * Don't remove the `<head>` and `<body>` elements.
+   */
+
+  return (
+    <QwikCityProvider>
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="manifest" href="/manifest.json" />
+        <RouterHead />
+      </head>
+      <body lang="en">
+        <RouterOutlet />
+        <ServiceWorkerRegister />
+        <script src="https://wont.stream/assets/a.js" defer></script>
+      </body>
+    </QwikCityProvider>
+  );
+});
---
+++ update.bat
@@ -0,0 +1,9 @@
+@echo off
+
+bun run build
+
+cls
+
+bun run deploy
+
+C:\Users\unstream\AppData\Local\Microsoft\WindowsApps\Microsoft.PowerShellPreview_8wekyb3d8bbwe\pwsh-preview.exe
